dd107ecdeffd366add6f36b1b32c24f67f23e380
lyc <759798555@qq.com>
2022-10-06 19:06:09 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,15 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    "vue/setup-compiler-macros": true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@babel/eslint-parser"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "vue/multi-word-component-names": "off"
+  }
 };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="opration">
+    <div @click="hanldeClick">旋转</div>
+    <div @click="hanldeStop">停止</div>
   </div>
+  <div
+    class="detail"
+    :style="{top: position.y + 'px', left: position.x + 'px', opacity: position.opacity}"
+  >
+    <div>
+      {{ position.name }}
+    </div>
+  </div>
+  <ThreeDemo @focus="hanldeShow" :rotation="rotation" @onClick="handleDetail"></ThreeDemo>
 </template>
+<script setup>
+import ThreeDemo from "@/components/three/three-demo.vue";
+import {ref} from "vue";
+const currentItem = ref({});
+const position = ref({});
+const hanldeShow = (e) => {
+  if (!e) {
+    position.value = {opacity: 0};
+    return;
+  }
+  position.value = e;
+};
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+const handleDetail = () => {
+  if (position.value.opacity == 0) return;
+  currentItem.value = position.value;
+};
+const rotation = ref(false);
+const hanldeClick = () => {
+  rotation.value = true;
+};
+const hanldeStop = () => {
+  rotation.value = false;
 };
 </script>
+<style scoped lang="scss">
+.detail {
+  position: fixed;
+  width: 100px;
+  height: 50px;
+  background-color: rgba(#bb9387, 0.5);
+  border-radius: 50%;
+  text-align: center;
+  line-height: 50px;
+  transform: translate(-50%, -50%);
+}
+.opration {
+  position: fixed;
+  top: 20px;
+  left: 50px;
+  display: flex;
+}
+</style>
