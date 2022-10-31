c048e9ea8cfa3733926d63e5df2b316c895613cc
lyc <759798555@qq.com>
2022-10-31 21:56:08 +0800

    自适应

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from "./store";
 import "@/assets/css/index.css";
 import registerAntdv from "@/global/index";
 import {menuMapToRoutes} from "@/utils/map-menu";
+import "./libs/flexible.js"; //自适应
+
 const app = createApp(App);
 registerAntdv(app);
 const routes = menuMapToRoutes();
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
-const {defineConfig} = require("@vue/cli-service");
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: true,
-  lintOnSave: false
-});
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      // /*no*/ /*px*/生效
+      sass: {
+        sassOptions: {
+          outputStyle: "expanded"
+        }
+      },
+      postcss: {
+        plugins: [
+          require("postcss-px2rem-exclude")({
+            // 以设计稿750为例， 750 / 10 = 75
+            remUnit: 192,
+            exclude: /node_modules/i
+          })
+        ]
+      }
+    }
+  }
+};
